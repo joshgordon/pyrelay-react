cb8115853db202d12a0d4e5b92208e8fb4163dc8
Enrique Caballero <ecaballerojr@icloud.com>
2017-02-21 23:59:25 -0500

    Testing out browserHistory

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React from 'react';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../store/';
 import {
@@ -17,7 +17,7 @@ const App = ({ children }: any): any => children;
 export default function () {
   return (
     <Provider store={store}>
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         <Route path={location.pathname} component={App}>
           <IndexRoute component={HelloWorld} />
         </Route>
