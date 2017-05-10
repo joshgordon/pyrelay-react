cddf4a1ef4a7757c57c9cb636231f1324294b15a
Enrique Caballero <enrique@visionistinc.com>
2017-05-10 16:12:12 -0400

    refactor: Ran prettier on server directory

--- server/express.js
+++ server/express.js
@@ -1,24 +1,24 @@
-import path from 'path';
-import express from 'express';
+import path from "path";
+import express from "express";
 
 const PORT_NUMBER = 3000;
-const DIST_DIR = path.resolve (__dirname, '..', 'dist');
-const MEDIA_DIR = path.resolve (__dirname, '..', 'media');
+const DIST_DIR = path.resolve (__dirname, "..", "dist");
+const MEDIA_DIR = path.resolve (__dirname, "..", "media");
 
 /** Instantiate your
     express server instance **/
 const server = express ();
 
-server.set ('json spaces', 2);
+server.set ("json spaces", 2);
 
 /** Tells express to serve files
     located in your build directory -> ../dist/ **/
-server.use ('/', express.static (DIST_DIR));
-server.use ('/resources', express.static (MEDIA_DIR));
-server.use ('*', express.static (DIST_DIR));
+server.use ("/", express.static (DIST_DIR));
+server.use ("/resources", express.static (MEDIA_DIR));
+server.use ("*", express.static (DIST_DIR));
 
 /** Start your server instance,
     and point your browser to localhost:PORT_NUMBER **/
 server.listen (PORT_NUMBER, () => {
-  console.info ('Express is listening on port', PORT_NUMBER);
+  console.info ("Express is listening on port", PORT_NUMBER);
 });
